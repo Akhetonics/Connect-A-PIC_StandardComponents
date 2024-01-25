3316be51461863b77efc4aaaf9309a1d14f4dc79
Max Aigner <max_aigneraigner@web.de>
2024-01-25 09:13:35 +0100

    fixed PCKs to have proper connection-weights

--- FindAndBuildAllPCKs.ps1
+++ FindAndBuildAllPCKs.ps1
@@ -1,21 +1,19 @@
-# Verwenden der Umgebungsvariablen für den Pfad zum Godot-Executable
-$godotExecutable = $env:GODOT
-$rootDirectory = "." # Stammverzeichnis für die Suche
+# get the path to the executable of godot using an environment variable
+$godotPath = $env:GODOT
+$rootDirectory = "." # the main directory where we start searching for project.godot files
 
-# Pfad zum Verzeichnis des PowerShell-Skripts
+# define the directory where to output the compiled *.pck files 
 $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 Get-ChildItem -Path $rootDirectory -Recurse -Filter "project.godot" | ForEach-Object {
-
-    # create the .godot folder
-    $godotArgs = "--headless", "--build-solutions", "--quit || exit 0"
-    $godotProcess = Start-Process -FilePath $godotPath -ArgumentList $godotArgs -PassThru
-    $godotProcess.WaitForExit()
 
     $projectPath = $_.FullName
     $projectDirectory = Split-Path $projectPath
     $folderName = Split-Path $projectDirectory -Leaf
     $exportPath = Join-Path $scriptDirectory "$folderName.pck"
 
-    & $godotExecutable --headless --export-pack "Windows Desktop" "$exportPath" --path "$projectDirectory"
+    # first create the .godot folder for each project
+    & $godotPath --headless --build-solutions --quit --path "$projectDirectory"
+    # then bake the projects into .pck files
+    & $godotPath --headless --export-pack "Windows Desktop" "$exportPath" --path "$projectDirectory"
 }
